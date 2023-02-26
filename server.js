24d4584be3e8fde0aea1c14bc5144ea580c5f586
y045u <fukasawa0405.tech@gmail.com>
2023-02-26 21:23:49 +0900

    auth postsの追加

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const app = express();
 const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+const postRoute = require("./routes/posts");
 const PORT = 3000;
 
 //ミドルウェア
 app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/posts", postRoute);
 
 app.get("/", (req, res) => {
     res.send("hello express");
