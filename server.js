a6c6de43d78453c7e446c73e9c5d24a75d0f7dc3
y045u <fukasawa0405.tech@gmail.com>
2023-02-26 20:32:39 +0900

    routing設定

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const app = express();
+const userRoute = require("./routes/users");
 const PORT = 3000;
 
+//ミドルウェア
+app.use("/api/users", userRoute);
+
 app.get("/", (req, res) => {
-    res.send("hello express")
-})
+    res.send("hello express");
+});
+
+// app.get("/users", (req, res) => {
+//     res.send("hello User")
+// })
 
 app.listen(PORT, () => console.log("サーバー起動しました"));
