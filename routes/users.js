c513d64066f5dc9cf79d82699c0208552b937a3a
y045u <fukasawa0405.tech@gmail.com>
2023-03-06 01:44:34 +0900

    ユーザー情報の取得

--- routes/users.js
+++ routes/users.js
@@ -27,7 +27,7 @@ router.put("/:id", async (req, res) => {
 
 //ユーザー情報の削除
 router.delete("/:id", async (req, res) => {
-    if(req.body.userId = req.params.id || req.body.isAdmin) {
+    if (req.body.userId = req.params.id || req.body.isAdmin) {
         try {
             const user = await User.findByIdAndDelete(req.params.id,);
             res.status(200).json("ユーザー情報が削除されました")
@@ -43,7 +43,15 @@ router.delete("/:id", async (req, res) => {
 
 
 //ユーザー情報の取得
-
+router.get("/:id", async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        const { password, updatedAt, ...other } = user._doc;
+        res.status(200).json(other);
+    } catch (err) {
+        res.stasus(500).json(err);
+    }
+});
 
 
 module.exports = router;
