f339cc449f0195cdca0e569e5185d1cdab7b7459
y045u <fukasawa0405.tech@gmail.com>
2023-03-06 01:22:37 +0900

    ユーザー情報の削除

--- routes/users.js
+++ routes/users.js
@@ -25,13 +25,26 @@ router.put("/:id", async (req, res) => {
 });
 
 
-
-
-
-
-
 //ユーザー情報の削除
+router.delete("/:id", async (req, res) => {
+    if(req.body.userId = req.params.id || req.body.isAdmin) {
+        try {
+            const user = await User.findByIdAndDelete(req.params.id,);
+            res.status(200).json("ユーザー情報が削除されました")
+        } catch (err) {
+            res.status(500).json(err)
+        }
+    } else {
+        return res
+            .status(403)
+            .json("あなたは自分のアカウントの時だけを削除できます")
+    }
+});
+
+
 //ユーザー情報の取得
+
+
 
 module.exports = router;
 
