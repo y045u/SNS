c476357abc457b498a78ea3a351ae5b25a370b79
y045u <fukasawa0405.tech@gmail.com>
2023-03-06 00:59:11 +0900

    ユーザー情報の更新

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,37 @@
 const router = require("express").Router();
-
+const User = require("../models/User");
 
 // router.get("/", (req, res) => {
 //     res.send("user router");
 // });
 
+//CRUD
+//ユーザー情報の更新
+router.put("/:id", async (req, res) => {
+    if (req.body.userId === req.params.id || req.body.isAdmin) {
+        try {
+            const user = await User.findByIdAndUpdate(req.params.id, {
+                $set: req.body,
+            });
+            res.status(200).json("ユーザー情報が更新されました");
+        } catch (err) {
+            return res.status(500).json(err);
+        }
+    } else {
+        return res
+            .status(403)
+            .json("あなたは自分のアカウントの時だけ情報を更新できます");
+    }
+});
 
+
+
+
+
+
+
+//ユーザー情報の削除
+//ユーザー情報の取得
 
 module.exports = router;
 
